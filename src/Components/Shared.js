b41dbead580f76e1c9ed45c495751cbc6a07393c
padp <solanavending@tutamail.com>
2025-03-08 05:55:59 -0600

    refactorization commit

--- src/Components/Shared.js
+++ src/Components/Shared.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://server-qftn.onrender.com";
+export const API_URL = "https://api.solvend.fun";
 export const JUP_SWAP_API_URL = 'https://quote-api.jup.ag/v6/quote';
 export const JUP_PRICE_API_URL = 'https://api.jup.ag/price/v2?ids=';
 export const PRICE_BUFFER_PREMIA = 0.1 // Percent increase in ask to ensure we clear minimum price
--- src/Vendor Components/EmployerComponents/Phone.js
+++ src/Vendor Components/EmployerComponents/Phone.js
@@ -1,19 +1,23 @@
 import React from "react";
+import './Phone.css'
 
 const Phone = ({ children, marginBottom = 0 }) => {
-    return (
-        <div className='mobile-demo-wrapper' style={{marginBottom: `${marginBottom}px`}}>
-            <div className='phone-outline' style={{ position: 'absolute' }}>
-                <div className='screen-outline'>
-                    <div className='speaker-outline'></div>
-                    <div className='volume-up-button'></div>
-                    <div className='volume-down-button'></div>
-                    <div className='power-on-button'></div>
-                    {children}
-                </div>
-            </div>
+  return (
+    <div
+      className="mobile-demo-wrapper"
+      style={{ marginBottom: `${marginBottom}px` }}
+    >
+      <div className="phone-outline" style={{ position: "absolute" }}>
+        <div className="screen-outline">
+          <div className="speaker-outline"></div>
+          <div className="volume-up-button"></div>
+          <div className="volume-down-button"></div>
+          <div className="power-on-button"></div>
+          <div className="phone-menu-contents">{children}</div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Phone;
